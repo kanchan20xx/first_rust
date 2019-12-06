c88cda34d71c511da9d26d2d49270f7e94689e90
kambayashi <kanchan20xx@gmail.com>
2019-12-06 22:00:18 +0900

    add std::io system.

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    // Display Hello, world!
     println!("Hello, world!");
+    println!("You should input number!");
+    // let : immutable 変数を作る.
+    // mut : mutable変数に昇格
+    // String型の変数作成。new()はstatic関数。(と思う。)
+    let mut input = String::new();
+    // 標準入力から、一行分読み取り -> inputに格納。
+    // & は参照
+    // expectはio::Resultオブジェクトのメソッド。
+    // io::ResultがErrのときに、引数のメッセージを表示。
+    std::io::stdin().read_line(&mut input).expect("Fail");
+    println! ("Your input is {}", input);
 }
